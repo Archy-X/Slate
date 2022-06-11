3a1abb21de15fa66ca92f9a26aeb3b36f680cf78
Archy-X <archydevelopment@gmail.com>
2022-06-11 12:10:41 -0700

    Change getStyle to use section symbol

--- src/main/java/com/archyx/slate/util/LoreUtil.java
+++ src/main/java/com/archyx/slate/util/LoreUtil.java
@@ -3,10 +3,10 @@ package com.archyx.slate.util;
 public class LoreUtil {
 
     public static String getStyle(String input) {
-        int firstColorCode = input.indexOf("&");
+        int firstColorCode = input.indexOf("§");
         if (firstColorCode != -1) {
             int currentPos = firstColorCode;
-            while (input.charAt(currentPos) == '&') {
+            while (input.charAt(currentPos) == '§') {
                 currentPos += 2;
             }
             return input.substring(firstColorCode, currentPos);
