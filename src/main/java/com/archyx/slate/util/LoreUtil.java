516dcfec11853737120598757bd7e07d9be240c0
Archy-X <archydevelopment@gmail.com>
2022-06-11 12:01:43 -0700

    Fix index out of bounds exception

--- src/main/java/com/archyx/slate/util/LoreUtil.java
+++ src/main/java/com/archyx/slate/util/LoreUtil.java
@@ -4,11 +4,15 @@ public class LoreUtil {
 
     public static String getStyle(String input) {
         int firstColorCode = input.indexOf("&");
-        int currentPos = firstColorCode;
-        while (input.charAt(currentPos) == '&') {
-            currentPos += 2;
+        if (firstColorCode != -1) {
+            int currentPos = firstColorCode;
+            while (input.charAt(currentPos) == '&') {
+                currentPos += 2;
+            }
+            return input.substring(firstColorCode, currentPos);
+        } else {
+            return "";
         }
-        return input.substring(firstColorCode, currentPos);
     }
 
 }
