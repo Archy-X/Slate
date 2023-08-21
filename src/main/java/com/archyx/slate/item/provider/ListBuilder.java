f044c9d3e783048f72d53cbc0663154a5bebcf6f
Archy-X <archydevelopment@gmail.com>
2023-08-20 23:06:54 -0700

    Add replacement varargs to ListBuilder#append

--- src/main/java/com/archyx/slate/item/provider/ListBuilder.java
+++ src/main/java/com/archyx/slate/item/provider/ListBuilder.java
@@ -1,5 +1,7 @@
 package com.archyx.slate.item.provider;
 
+import com.archyx.slate.util.TextUtil;
+
 public class ListBuilder {
 
     private final ListInsertion insertion;
@@ -9,7 +11,10 @@ public class ListBuilder {
         this.insertion = insertion;
     }
 
-    public ListBuilder append(String text) {
+    public ListBuilder append(String text, String... rep) {
+        if (rep != null) {
+            text = replace(text, rep);
+        }
         if (list == null) {
             list = text;
         } else {
@@ -20,6 +25,19 @@ public class ListBuilder {
 
     public String build() {
         return list;
+    }
+
+    private String replace(String source, String... rep) {
+        if (source == null) {
+            return null;
+        }
+        if (rep.length % 2 != 0) {
+            throw new IllegalArgumentException("The number of arguments must be even!");
+        }
+        for (int i = 0; i < rep.length; i += 2) {
+            source = TextUtil.replace(source, rep[i], rep[i + 1]);
+        }
+        return source;
     }
 
 }
