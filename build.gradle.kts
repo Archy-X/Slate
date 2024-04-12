13d73275acffc0ae673f730de5cc4196b6440eb9
Archy-X <archydevelopment@gmail.com>
2024-04-12 18:45:18 -0400

    Make item-nbt-api compileOnly

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     }
     implementation("net.kyori:adventure-text-minimessage:4.16.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.2")
-    implementation("de.tr7zw:item-nbt-api:2.11.3")
+    compileOnly("de.tr7zw:item-nbt-api:2.11.3")
     compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.10.10")
     compileOnly("com.mojang:authlib:1.5.25")
